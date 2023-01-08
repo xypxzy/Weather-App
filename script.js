74da1dcbde95627d0394719fe1800738dd957007
xypxzy <xypxzy@gmail.com>
2023-01-08 12:19:45 +0600

    Finished search city block

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const container = document.querySelector('.container');
+const weatherContent = document.querySelector('.weatherContent');
+
 
 let weather = {
     'apiKeys': '4e0f733bcb83269e7da9165f48542178',
@@ -19,39 +21,55 @@ let weather = {
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
         console.log(name, icon, description, temp, humidity, speed, country)
+
+        weatherContent.classList.add('hidden')
+
         const html = `
             <div class="weatherContentWindow">
                 <div class="headerBlock">                
+                    <span class="material-symbols-outlined backBtn">arrow_back</span>
                     <p class="contentText"> Weather App</p>
-                    <span class="material-symbols-outlined">arrow_back</span>
                 </div>
                 <div class="line"></div>
                 <div class="innerBlock">
-                    <img class="icon" alt="Icons">
-                    <div class="temperature">${(temp - 273).toFixed(2)} C</div>
+                    <img class="icon" alt="Icons" width="120px">
+                    <div class="temperature">${(temp - 273).toFixed(2)}&degC</div>
                     <div class="description">${capitalize(description)}</div>
                     <div class="city country">${name}, ${country}</div>
                 </div>
                 <div class="line"></div>
                 <div class="floorBlock">
                     <div class="speedWind">
-                        <p>${speed} mps</p>s
-                        <p>Wind Speed</p>
+                        <img src="./windSpeedIcon.png" alt="wind speed icon" width="30px">
+                        <div class="speedInfo">
+                            <p>${speed} mps</p>
+                            <p>Wind Speed</p>
+                        </div>
                     </div>
                     <div class="humidity">
-                        <p>${humidity}%</p>
-                        <p>Humadity</p>
+                        <img src="./humadityIcon.png" alt="humidity icon" width="30px">
+                        <div class="humidityInfo">
+                            <p>${humidity}%</p>
+                            <p>Humadity</p>
+                        </div>
                     </div>
                 </div>`;
         container.insertAdjacentHTML('beforeend', html)
         document.querySelector('.icon').src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
+        document.querySelector('.backBtn').addEventListener('click', arrowBackBtn);
+
     }
 }
 
+/*Button Arrow Back*/
 
-weather.fetchWeather('Tokyo')
+function arrowBackBtn(event) {
+    document.querySelector('.weatherContentWindow').classList.add('hidden');
+    weatherContent.classList.remove('hidden');
+}
 
+/*Capitalizing every word in the string*/
 function capitalize(str) {
     return str.split(' ').map((word) => `${word.slice(0, 1).toUpperCase()}${word.slice(1)}`).join(' ')
 
